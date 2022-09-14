a880d1f588b761a74c37e189287748f0b763674a
littleGnAl <tearjeaker@hotmail.com>
2022-09-14 19:59:44 +0800

    fix: fix AudioFrameObserver event bugs

--- example/lib/examples/advanced/process_audio_raw_data/process_audio_raw_data.dart
+++ example/lib/examples/advanced/process_audio_raw_data/process_audio_raw_data.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:agora_rtc_engine_example/config/agora.config.dart' as config;
 import 'package:agora_rtc_engine_example/components/example_actions_widget.dart';
 import 'package:agora_rtc_engine_example/components/log_sink.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 /// ProcessVideoRawData Example
 class ProcessAudioRawData extends StatefulWidget {
@@ -24,6 +28,7 @@ class _State extends State<ProcessAudioRawData> {
   bool _isUseFlutterTexture = false;
   ChannelProfileType _channelProfileType =
       ChannelProfileType.channelProfileLiveBroadcasting;
+  late File _audioFile;
 
   @override
   void initState() {
@@ -89,10 +94,25 @@ class _State extends State<ProcessAudioRawData> {
 
     await _engine.enableVideo();
 
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    _audioFile = File(path.join(appDocDir.absolute.path, 'audio.raw'));
+    if (await _audioFile.exists()) {
+      await _audioFile.delete();
+    }
+    await _audioFile.create();
+    logSink.log('Audio file output to: ${_audioFile.absolute.path}');
+
     _engine.getMediaEngine().registerAudioFrameObserver(AudioFrameObserver(
-      onRecordAudioFrame: (channelId, audioFrame) {
+      onRecordAudioFrame: (channelId, audioFrame) async {
         debugPrint(
             '[onRecordAudioFrame] channelId: $channelId, audioFrame: ${audioFrame.toJson()}');
+        if (!isJoined) {
+          return;
+        }
+        if (audioFrame.buffer != null) {
+          await _audioFile.writeAsBytes(audioFrame.buffer!.toList(),
+              mode: FileMode.append, flush: true);
+        }
       },
     ));
 
@@ -139,39 +159,8 @@ class _State extends State<ProcessAudioRawData> {
     return ExampleActionsWidget(
       displayContentBuilder: (context, isLayoutHorizontal) {
         if (!_isReadyPreview) return Container();
-        return Stack(
-          children: [
-            AgoraVideoView(
-              controller: VideoViewController(
-                rtcEngine: _engine,
-                canvas: const VideoCanvas(uid: 0),
-                useFlutterTexture: _isUseFlutterTexture,
-              ),
-            ),
-            Align(
-              alignment: Alignment.topLeft,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: List.of(remoteUid.map(
-                    (e) => SizedBox(
-                      width: 120,
-                      height: 120,
-                      child: AgoraVideoView(
-                        controller: VideoViewController.remote(
-                          rtcEngine: _engine,
-                          canvas: VideoCanvas(uid: e),
-                          connection:
-                              RtcConnection(channelId: _controller.text),
-                          useFlutterTexture: _isUseFlutterTexture,
-                        ),
-                      ),
-                    ),
-                  )),
-                ),
-              ),
-            )
-          ],
+        return const Center(
+          child: Text('No Preview'),
         );
       },
       actionsBuilder: (context, isLayoutHorizontal) {
