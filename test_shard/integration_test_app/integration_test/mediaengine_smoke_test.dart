8908cf9bad13cbb6b6d9d3233089d8dcf9e1d4e1
littleGnAl <tearjeaker@hotmail.com>
2022-09-27 19:59:56 +0800

    WIP

--- test_shard/integration_test_app/integration_test/mediaengine_smoke_test.dart
+++ test_shard/integration_test_app/integration_test/mediaengine_smoke_test.dart
@@ -1,8 +1,132 @@
+import 'dart:async';
+
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/foundation.dart';
 import 'package:integration_test/integration_test.dart';
 import 'generated/mediaengine_smoke_test.generated.dart' as generated;
+import 'package:integration_test_app/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   generated.mediaEngineSmokeTestCases();
+
+  testWidgets(
+    'registerAudioFrameObserver',
+    (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      String engineAppId = const String.fromEnvironment('TEST_APP_ID',
+          defaultValue: '<YOUR_APP_ID>');
+
+      RtcEngine rtcEngine = createAgoraRtcEngine();
+      await rtcEngine.initialize(RtcEngineContext(
+        appId: engineAppId,
+        areaCode: AreaCode.areaCodeGlob.value(),
+      ));
+
+      final mediaEngine = rtcEngine.getMediaEngine();
+      Completer<bool>? eventCalledCompleter = Completer();
+      final AudioFrameObserver observer = AudioFrameObserver(
+        onRecordAudioFrame: (String channelId, AudioFrame audioFrame) {
+          if (eventCalledCompleter == null) return;
+          eventCalledCompleter.complete(true);
+        },
+        onPlaybackAudioFrame: (String channelId, AudioFrame audioFrame) {},
+        onMixedAudioFrame: (String channelId, AudioFrame audioFrame) {},
+        onPlaybackAudioFrameBeforeMixing:
+            (String channelId, int uid, AudioFrame audioFrame) {},
+      );
+      mediaEngine.registerAudioFrameObserver(
+        observer,
+      );
+
+      await rtcEngine.enableVideo();
+
+      await rtcEngine.joinChannel(
+        token: '',
+        channelId: 'testonaction',
+        uid: 0,
+        options: const ChannelMediaOptions(
+          channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+          clientRoleType: ClientRoleType.clientRoleBroadcaster,
+        ),
+      );
+
+      final eventCalled = await eventCalledCompleter.future;
+      expect(eventCalled, isTrue);
+      eventCalledCompleter = null;
+
+      mediaEngine.unregisterAudioFrameObserver(observer);
+      await rtcEngine.leaveChannel();
+
+      await mediaEngine.release();
+      await rtcEngine.release();
+    },
+  );
+
+  testWidgets(
+    'registerVideoFrameObserver',
+    (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      String engineAppId = const String.fromEnvironment('TEST_APP_ID',
+          defaultValue: '<YOUR_APP_ID>');
+
+      RtcEngine rtcEngine = createAgoraRtcEngine();
+      await rtcEngine.initialize(RtcEngineContext(
+        appId: engineAppId,
+        areaCode: AreaCode.areaCodeGlob.value(),
+      ));
+
+      final mediaEngine = rtcEngine.getMediaEngine();
+      Completer<bool>? eventCalledCompleter = Completer();
+
+      final VideoFrameObserver observer = VideoFrameObserver(
+        onCaptureVideoFrame: (videoFrame) {
+          debugPrint(
+              '[onCaptureVideoFrame] videoFrame: ${videoFrame.toJson()}');
+
+          if (eventCalledCompleter == null) return;
+          eventCalledCompleter.complete(true);
+        },
+        onRenderVideoFrame:
+            (String channelId, int remoteUid, VideoFrame videoFrame) {
+          // logSink.log(
+          //     '[onRenderVideoFrame] channelId: $channelId, remoteUid: $remoteUid, videoFrame: ${videoFrame.toJson()}');
+          debugPrint(
+              '[onRenderVideoFrame] channelId: $channelId, remoteUid: $remoteUid, videoFrame: ${videoFrame.toJson()}');
+        },
+      );
+
+      mediaEngine.registerVideoFrameObserver(
+        observer,
+      );
+
+      await rtcEngine.enableVideo();
+
+      await rtcEngine.joinChannel(
+        token: '',
+        channelId: 'testonaction',
+        uid: 0,
+        options: const ChannelMediaOptions(
+          channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+          clientRoleType: ClientRoleType.clientRoleBroadcaster,
+        ),
+      );
+
+      final eventCalled = await eventCalledCompleter.future;
+      expect(eventCalled, isTrue);
+      eventCalledCompleter = null;
+
+      mediaEngine.unregisterVideoFrameObserver(observer);
+      await rtcEngine.leaveChannel();
+
+      await mediaEngine.release();
+      await rtcEngine.release();
+    },
+  );
 }
--- test_shard/integration_test_app/integration_test/rtcengine_smoke_test.dart
+++ test_shard/integration_test_app/integration_test/rtcengine_smoke_test.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/foundation.dart';
@@ -56,6 +58,64 @@ void main() {
         debugPrint(
             '[getEffectDuration] errorcode: ${(e as AgoraRtcException).code}');
       }
+
+      await rtcEngine.release();
+    },
+//  skip: !(),
+  );
+
+  testWidgets(
+    'registerAudioEncodedFrameObserver',
+    (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+
+      String engineAppId = const String.fromEnvironment('TEST_APP_ID',
+          defaultValue: '<YOUR_APP_ID>');
+
+      RtcEngine rtcEngine = createAgoraRtcEngine();
+      await rtcEngine.initialize(RtcEngineContext(
+        appId: engineAppId,
+        areaCode: AreaCode.areaCodeGlob.value(),
+      ));
+      await rtcEngine.enableVideo();
+
+      Completer<bool>? eventCalledCompleter = Completer();
+      final AudioEncodedFrameObserver observer = AudioEncodedFrameObserver(
+        onRecordAudioEncodedFrame: (Uint8List frameBuffer, int length,
+            EncodedAudioFrameInfo audioEncodedFrameInfo) {
+          if (eventCalledCompleter == null) return;
+          eventCalledCompleter.complete(true);
+        },
+        onPlaybackAudioEncodedFrame: (Uint8List frameBuffer, int length,
+            EncodedAudioFrameInfo audioEncodedFrameInfo) {},
+        onMixedAudioEncodedFrame: (Uint8List frameBuffer, int length,
+            EncodedAudioFrameInfo audioEncodedFrameInfo) {},
+      );
+      rtcEngine.registerAudioEncodedFrameObserver(
+          config: const AudioEncodedFrameObserverConfig(
+              postionType: AudioEncodedFrameObserverPosition
+                  .audioEncodedFrameObserverPositionRecord,
+              encodingType: AudioEncodingType.audioEncodingTypeAac16000Medium),
+          observer: observer);
+
+      await rtcEngine.joinChannel(
+        token: '',
+        channelId: 'testonaction',
+        uid: 0,
+        options: const ChannelMediaOptions(
+          channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+          clientRoleType: ClientRoleType.clientRoleBroadcaster,
+        ),
+      );
+
+      final eventCalled = await eventCalledCompleter.future;
+      expect(eventCalled, isTrue);
+      eventCalledCompleter = null;
+
+      rtcEngine.unregisterAudioEncodedFrameObserver(
+        observer,
+      );
 
       await rtcEngine.release();
     },
