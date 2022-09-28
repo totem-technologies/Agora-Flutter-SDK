f722e3ce9ecbde917b5ed3db17759f6bdf0506e8
littleGnAl <tearjeaker@hotmail.com>
2022-09-28 20:01:15 +0800

    WIP

--- test_shard/integration_test_app/integration_test/rtcengine_event_handler_test.dart
+++ test_shard/integration_test_app/integration_test/rtcengine_event_handler_test.dart
@@ -1,103 +1,102 @@
-// import 'dart:async';
-// import 'dart:convert';
-// import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
-// import 'package:agora_rtc_engine/agora_rtc_engine.dart';
-// import 'package:flutter/foundation.dart';
-// import 'package:flutter/services.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:integration_test/integration_test.dart';
-// import 'package:iris_tester/iris_tester.dart';
-// import 'package:path_provider/path_provider.dart';
-// import 'generated/rtcengine_smoke_test.generated.dart' as generated;
-// import 'package:integration_test_app/main.dart' as app;
-// import 'package:path/path.dart' as path;
-// import 'package:agora_rtc_engine/src/impl/api_caller.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:path_provider/path_provider.dart';
+import 'generated/rtcengine_smoke_test.generated.dart' as generated;
+import 'package:integration_test_app/main.dart' as app;
+import 'package:path/path.dart' as path;
+import 'package:agora_rtc_engine/src/impl/api_caller.dart';
 
-// void main() {
-//   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-//   testWidgets(
-//     'onJoinChannelSuccess',
-//     (WidgetTester tester) async {
-//       app.main();
-//       await tester.pumpAndSettle();
+  testWidgets(
+    'onJoinChannelSuccess',
+    (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
 
-//       String engineAppId = const String.fromEnvironment('TEST_APP_ID',
-//           defaultValue: '<YOUR_APP_ID>');
+      // String engineAppId = const String.fromEnvironment('TEST_APP_ID',
+      //     defaultValue: '<YOUR_APP_ID>');
 
-//       final irisTester = IrisTester();
-//       final debugApiEngineIntPtr = irisTester.createDebugApiEngine();
-//       setMockIrisApiEngineIntPtr(debugApiEngineIntPtr);
+      // final irisTester = IrisTester();
+      // final debugApiEngineIntPtr = irisTester.createDebugApiEngine();
+      // setMockIrisApiEngineIntPtr(debugApiEngineIntPtr);
 
-//       RtcEngine rtcEngine = createAgoraRtcEngine();
-//       await rtcEngine.initialize(RtcEngineContext(
-//         appId: engineAppId,
-//         areaCode: AreaCode.areaCodeGlob.value(),
-//       ));
+      // RtcEngine rtcEngine = createAgoraRtcEngine();
+      // await rtcEngine.initialize(RtcEngineContext(
+      //   appId: engineAppId,
+      //   areaCode: AreaCode.areaCodeGlob.value(),
+      // ));
 
-//       final Completer<bool> calledEventCompleter = Completer();
-//       final eventHandler = RtcEngineEventHandler(
-//         onJoinChannelSuccess: (connection, elapsed) {
-//           calledEventCompleter.complete(true);
-//         },
-//       );
-//       rtcEngine.registerEventHandler(eventHandler);
+      // final Completer<bool> calledEventCompleter = Completer();
+      // final eventHandler = RtcEngineEventHandler(
+      //   onJoinChannelSuccess: (connection, elapsed) {
+      //     calledEventCompleter.complete(true);
+      //   },
+      // );
+      // rtcEngine.registerEventHandler(eventHandler);
 
-//       await Future.delayed(const Duration(seconds: 1));
+      // await Future.delayed(const Duration(seconds: 1));
 
-//       irisTester.fireEvent('');
+      // irisTester.fireEvent('');
 
-//       final calledEvent = await calledEventCompleter.future;
+      // final calledEvent = await calledEventCompleter.future;
 
-//       expect(calledEvent, isTrue);
+      // expect(calledEvent, isTrue);
 
-//       rtcEngine.unregisterEventHandler(eventHandler);
+      // rtcEngine.unregisterEventHandler(eventHandler);
 
-//       await rtcEngine.release();
-//     },
-// //  skip: !(),
-//   );
+      // await rtcEngine.release();
+    },
+//  skip: !(),
+  );
 
-//   testWidgets(
-//     'onRejoinChannelSuccess',
-//     (WidgetTester tester) async {
-//       app.main();
-//       await tester.pumpAndSettle();
+  testWidgets(
+    'onRejoinChannelSuccess',
+    (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
 
-//       String engineAppId = const String.fromEnvironment('TEST_APP_ID',
-//           defaultValue: '<YOUR_APP_ID>');
+      // String engineAppId = const String.fromEnvironment('TEST_APP_ID',
+      //     defaultValue: '<YOUR_APP_ID>');
 
-//       final irisTester = IrisTester();
-//       final debugApiEngineIntPtr = irisTester.createDebugApiEngine();
+      // final irisTester = IrisTester();
+      // final debugApiEngineIntPtr = irisTester.createDebugApiEngine();
 
-//       setMockIrisApiEngineIntPtr(debugApiEngineIntPtr);
-//       RtcEngine rtcEngine = createAgoraRtcEngine();
-//       await rtcEngine.initialize(RtcEngineContext(
-//         appId: engineAppId,
-//         areaCode: AreaCode.areaCodeGlob.value(),
-//       ));
+      // setMockIrisApiEngineIntPtr(debugApiEngineIntPtr);
+      // RtcEngine rtcEngine = createAgoraRtcEngine();
+      // await rtcEngine.initialize(RtcEngineContext(
+      //   appId: engineAppId,
+      //   areaCode: AreaCode.areaCodeGlob.value(),
+      // ));
 
-//       final Completer<bool> calledEventCompleter = Completer();
-//       final eventHandler = RtcEngineEventHandler(
-//         onRejoinChannelSuccess: (connection, elapsed) {
-//           calledEventCompleter.complete(true);
-//         },
-//       );
-//       rtcEngine.registerEventHandler(eventHandler);
+      // final Completer<bool> calledEventCompleter = Completer();
+      // final eventHandler = RtcEngineEventHandler(
+      //   onRejoinChannelSuccess: (connection, elapsed) {
+      //     calledEventCompleter.complete(true);
+      //   },
+      // );
+      // rtcEngine.registerEventHandler(eventHandler);
 
-//       await Future.delayed(const Duration(seconds: 1));
+      // await Future.delayed(const Duration(seconds: 1));
 
-//       irisTester.fireEvent('');
+      // irisTester.fireEvent('');
 
-//       final calledEvent = await calledEventCompleter.future;
+      // final calledEvent = await calledEventCompleter.future;
 
-//       expect(calledEvent, isTrue);
+      // expect(calledEvent, isTrue);
 
-//       rtcEngine.unregisterEventHandler(eventHandler);
+      // rtcEngine.unregisterEventHandler(eventHandler);
 
-//       await rtcEngine.release();
-//     },
-// //  skip: !(),
-//   );
-// }
+      // await rtcEngine.release();
+    },
+//  skip: !(),
+  );
+}
